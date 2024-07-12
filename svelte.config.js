fa8ae0bcb20b929b98bc336beede32c54c001e41
Juan Quenga <110294740+JuanQuenga@users.noreply.github.com>
2024-07-12 09:02:50 -0400

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -12,11 +12,6 @@ const config = {
     // If your environment is not supported, or you settled on a specific environment, switch out the adapter.
     // See https://kit.svelte.dev/docs/adapters for more information about adapters.
     adapter: adapter(),
-    routes: (route) => {
-      if (route.startsWith("/.well-known")) {
-        return route;
-      }
-    },
   },
 };
 
